2475d45bc82b8473fd3aa4051bc79c069081832a
yashwithpoojari <128124363+yashwithpoojari@users.noreply.github.com>
2023-03-21 02:20:53 -0700

    Create se2.java
    creating java file

---
+++ se2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class labprogram23{
+	public static void main(String[]args){
+	 int f1=0,f2=1,f3,i,n;
+	 Scanner s=new Scanner(System.in);
+	 System.out.print("Enter the number:");
+	 n=s.nextInt();
+	 System.out.println(f1);
+	 System.out.println(f2);
+	 for(i=2;i<=n;i++){
+	 f3=f1+f2;
+	 System.out.println(f3);
+	 f1=f2;
+	 f2=f3;
+  }
+ }
+}	
